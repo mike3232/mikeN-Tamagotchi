1daac3879f4a4faa50c3ae861927fa13ff90b194
mike3232 <micnad@sbcglobal.net>
2016-04-06 14:42:57 -0400

    I was able to make my first commit.

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tamagotchi t = new Tamagotchi("Mike ", "Male ",  15, 15);
+            Console.WriteLine("Name: " + t.getName());
+            Console.WriteLine("Gender: " + t.getGender());
+            Console.WriteLine("Fullness: " + t.getFullness());
+            Console.WriteLine("Happiness: " + t.getHappiness());
+
+            while (t.getFullness() > 0 && t.getHappiness() >= 0)
+            {
+                Console.WriteLine("Would you like to feed or pet your Tamagotchi?");
+                String action = Console.ReadLine();
+                if (action == "pet") //Happiness++
+                {
+                    t.pet();
+                }
+                else if (action == "feed")
+                {
+                    t.feed(); //Fullness++
+                }
+                else
+                {
+                    Console.WriteLine("You Screwed Up!!!!");
+                }
+                t.dostuff(); //tried to move, didn't work, tried to do if (action == "dostuff") but other code didn't work
+                Console.WriteLine("Your Tamagotchi's Fullness is {0} and Happiness is {1}", t.getFullness(), t.getHappiness());
+
+                if (t.getFullness() <=0 || t.getHappiness() <=0)
+                {
+                  Console.WriteLine("Na na na na. Na na na na. Hey hey hey....Goodbye");
+                }              
+            }
+        }
+    }
+}
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi  // idea petting and feeding cause values to go up
+{
+    class Tamagotchi
+    {
+        private string Name = "N/A ";
+        private string Gender = " N/A";
+        private int Fullness = 0;
+        private int Happiness = 0;
+
+        public Tamagotchi(string name, string gender, int fullness, int happiness)
+        {
+            Name = name; //lowercase "name" could have been called anything like Bee, but string name above would be string bee.
+            Gender = gender;
+            Fullness = fullness;
+            Happiness = happiness;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public string getGender()
+        {
+            return Gender;
+        }
+
+        public int getFullness()
+        {
+            return Fullness;
+        }
+        public int getHappiness()
+        {
+            return Happiness;
+        }
+        public int pet()
+        {
+            Happiness = Happiness + 1;
+            return Happiness;
+        }
+        public int feed()
+        {
+            Fullness = Fullness + 1;
+            return Fullness;
+        }
+        public void dostuff()
+        {
+            Random rnd = new Random();
+            Happiness = Happiness - rnd.Next(5); //rnd.Next will return a single integer by puttin
+            // a "5" random numbers will be between 0 to 5  
+            // Could also write rnd.Next(1,10); 1 starting value 10 ending value
+            Fullness = Fullness - rnd.Next(10);
+            Console.WriteLine(Happiness);
+            Console.WriteLine(Fullness);
+        }
+    }
+}
